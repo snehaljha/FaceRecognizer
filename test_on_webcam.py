7d7b0c4e77d3bc260ffdf5a69f6ef993818c1019
snehaljha <snehaljha08@gmail.com>
2020-05-20 11:59:14 +0530

    fixed error

--- test_on_webcam.py
+++ test_on_webcam.py
@@ -14,10 +14,10 @@ video_cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = video_cap.read()
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     boxes = face_recognition.face_locations(image, model='hog')
-    encodings = face_recognition.face_encodings(image, boxes)
-    if len(encodings)>0:
+    if len(boxes)>0:
+        encodings = face_recognition.face_encodings(image, boxes)
         result = model.predict(np.asarray(encodings).reshape(-1,128)).tolist()
         for i in range(len(boxes)):
             cv2.rectangle(frame, (boxes[0][3], boxes[0][0]), (boxes[0][1], boxes[0][2]), (255,0,0), 2)
